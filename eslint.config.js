74308341c299792ee0e839c3f1496513da7a4007
이원형 <kpss0337@gmail.com>
2026-05-14 14:54:32 +0900

    chore(Eslint + Prettier): add Lints
    - esLint, prettier 버전 수정 및 적용

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+// eslint.config.js
+import js from '@eslint/js'
+import vue from 'eslint-plugin-vue'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
+
+export default [
+  {
+    ignores: ['dist', 'node_modules']
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  ...vue.configs['flat/recommended'],
+
+  {
+    files: ['**/*.{ts,vue}'],
+
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+        ecmaVersion: 2020,
+        sourceType: 'module'
+      }
+    },
+
+    plugins: {
+      prettier: prettierPlugin
+    },
+
+    rules: {
+      'prettier/prettier': 'error',
+
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'off',
+
+      'vue/multi-word-component-names': 'off'
+    }
+  },
+
+  prettier
+]
